d69be6578de95710d4b7e7a1b9c259c9f7ba2291
Mateusz.Cholewa <mateusz.cholewa@asseco.pl>
2024-03-01 14:41:44 +0100

    dodanie klasy + gettery i settery

---
+++ MovieLibrary/src/main/java/org/example/models/Actors.java
@@ -0,0 +1,16 @@
+package org.example.models;
+
+public class Actors
+{
+    private String firstName;
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+}
